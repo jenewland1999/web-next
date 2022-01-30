55430fa5e716a556083891306eaaa2ac6fe693a9
jenewland1999 <github@jenewland.me.uk>
2022-01-30 10:40:59 +0000

    Add Google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,9 @@ import Document, {
   NextScript,
 } from "next/document";
 
+const GOOGLE_FONTS_URL =
+  "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap";
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -15,7 +18,34 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Fastest Google Fonts https://csswizardry.com/2020/05/the-fastest-google-fonts/ */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link rel="preload" as="style" href={GOOGLE_FONTS_URL} />
+          {/* https://github.com/vercel/next.js/issues/12984 ¯\_(ツ)_/¯ */}
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+      </style>
+        <link
+          rel="stylesheet"
+          href="${GOOGLE_FONTS_URL}"
+          media="print"
+          onload="this.media = 'all';"
+        />
+      <style>
+    `,
+            }}
+          ></style>
+          <noscript>
+            <link rel="stylesheet" href={GOOGLE_FONTS_URL} />
+          </noscript>
+        </Head>
         <body>
           <Main />
           <NextScript />
