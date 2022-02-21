94d87a77e1a5b0006b893a0f050813b52dd4287a
jenewland1999 <github@jenewland.me.uk>
2022-02-21 08:40:25 +0000

    Update default font to Inter variable

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const GOOGLE_FONTS_URL =
-  "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap";
+  "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap";
